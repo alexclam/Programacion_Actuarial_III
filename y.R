5e1081fa29f9dacc1dfaf7ed04d3c8a9b53a9a30
Alejandro Villegas <aguila_alex09@hotmail.com>
2017-02-26 16:40:32 -0600

    Dput- Dget
    Subimos archivos usados como practica de dput y dget

---
+++ y.R
@@ -0,0 +1,3 @@
+structure(list(a = 1:10, b = structure(c(1L, 1L, 1L, 1L, 1L, 
+1L, 1L, 1L, 1L, 1L), class = "factor", .Label = "a")), .Names = c("a", 
+"b"), row.names = c(NA, -10L), class = "data.frame")
